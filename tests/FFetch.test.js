2dc47218564129c91af625a360df1655b66c72bf
axross <kou@axross.org>
2015-11-18 11:09:23 +0900

    Add tests for FFetch.createFullUrl()

---
+++ tests/FFetch.test.js
@@ -0,0 +1,119 @@
+import test from 'tape';
+import querystring from 'querystring';
+import ffetch, { FFetch } from '../sources'
+
+test('FFetch.createFullUrl() helps to parse the URL using base, params and queries', t => {
+  const patterns = [
+    {
+      input: {
+        base: '/path/to/api/post/:id',
+        params: { id: 25 },
+        queries: { foo: 'bar' },
+      },
+      expected: '/path/to/api/post/25',
+      isNeedQueryString: true,
+    },
+    {
+      input: {
+        base: '/path/to/api/post/:id:/:id',
+        params: { id: 25 },
+        queries: {},
+      },
+      expected: '/path/to/api/post/25:/25',
+      isNeedQueryString: false,
+    },
+    {
+      input: {
+        base: '/path/to/api/post',
+        params: {},
+        queries: {
+          foo: 'bar',
+          baz: 'qux',
+        },
+      },
+      expected: '/path/to/api/post',
+      isNeedQueryString: true,
+    },
+    {
+      input: {
+        base: '/path/to/api/post/:id/comment/:commentId',
+        params: { id: 25, commentId: 5963 },
+        queries: {
+          foo: 'bar',
+          columns: ['id', 'title', 'body', 'createdAt'],
+          baz: 'qux',
+        },
+      },
+      expected: '/path/to/api/post/25/comment/5963',
+      isNeedQueryString: true,
+    },
+    {
+      input: {
+        base: 'a'
+      },
+      expected: 'a',
+      isNeedQueryString: false,
+    }
+  ];
+
+  t.plan(patterns.length);
+
+  patterns.forEach(({ input, expected, isNeedQueryString }) => {
+    t.equal(
+      FFetch.createFullUrl(input),
+      `${expected}${isNeedQueryString ? '?' : ''}${querystring.stringify(input.queries)}`
+    );
+  });
+});
+
+test('FFetch.createFullUrl() throws a TypeError if params is not a plain-object', t => {
+  const args = {
+    base: '',
+    queries: {},
+  };
+
+  const validPatterns = [
+    {},
+    { foo: 'bar' },
+    { foo: 12 },
+    undefined,
+  ];
+
+  const invalidPatterns = [
+    'str',
+    123,
+    true,
+    null,
+    [],
+    /^regexp$/,
+    new Date(),
+    () => {},
+  ];
+
+  t.plan(validPatterns.length + invalidPatterns.length);
+
+  FFetch.createFullUrl('base', {}, 'str');
+
+  validPatterns.forEach(params => {
+    t.doesNotThrow(() => {
+      return FFetch.createFullUrl(Object.assign({}, args, { params }));
+    });
+  });
+
+  invalidPatterns.forEach(params => {
+    t.throws(() => {
+      return FFetch.createFullUrl(Object.assign({}, args, { params }));
+    });
+  });
+});
+
+test('FFetch.createFullUrl() throws a TypeError if params includes a string of value it starts with ":"', t => {
+  t.plan(1);
+
+  t.throws(() => {
+    return FFetch.createFullUrl({
+      base: '',
+      params: { foo: ':bar' },
+    });
+  });
+});
--- tests/initialize.es6
+++
@@ -1,7 +0,0 @@
-import ES6Promise from 'es6-promise';
-import fetch from 'isomorphic-fetch';
-import expect from 'expect.js';
-
-global.Promise = ES6Promise.Promise;
-global.fetch = fetch;
-global.expect = expect;
